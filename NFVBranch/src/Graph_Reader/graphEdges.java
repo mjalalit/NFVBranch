3f7a2b7511eed4b0310011aa68e0ec6d73e0aace
Maryam Jalali <mj3821@gmail.com>
2022-04-10 23:45:19 -0700

    Update graphEdges.java

--- NFVBranch/src/Graph_Reader/graphEdges.java
+++ NFVBranch/src/Graph_Reader/graphEdges.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Graph_Reader;
 
 import Graph_Generator.Edge;
@@ -17,9 +12,6 @@ import java.util.Random;
 import Algorithms.UsefulMethods;
 
 
-/**
- * @author mjalalitabar1
- */
 public class graphEdges {
 
     private ArrayList<Edge> edges;
@@ -35,7 +27,8 @@ public class graphEdges {
     public void setEdges(ArrayList<Edge> edges) {
         this.edges = edges;
     }
-
+    //Receives a name of a file, reads the matrix in file, based on that create the edges for
+    //a weighted graph. BW represent the weight of each edge
     public ArrayList<graphEdge> getAllEdgesNew(String filename) {
         ArrayList<graphEdge> alledges = new ArrayList<graphEdge>();
         readFromFile rd = new readFromFile();
@@ -59,91 +52,31 @@ public class graphEdges {
 
     	return alledges;
     }
-
-    public ArrayList<graphEdge> getAllEdges(String filename) { //Get the edges
-
-        ArrayList<graphEdge> alledges = new ArrayList<graphEdge>();
-        int[][] matrix1;
-        int[][] matrix2;
+    
+    //Receives a name of a file, reads the matrix in file, based on that create the vertices for
+    //a  graph. Each node has id and a unit that represent the computational capacity (CPU)
+  public ArrayList<Vertex> getAllVerticesNew(int vnfs, String filename) {
+    	
         readFromFile rd = new readFromFile();
-        // matrix1=rd.readfiles("NSFNET14.txt");
-        matrix1 = rd.readfiles(filename);
-
-
-        matrix2 = matrix1;
-        int numOfVertices;
-        numOfVertices = matrix1[0].length;
-        //CPUmatrix=new int[numOfVertices][numOfVertices];
-        int startIndexBW = numOfVertices;
-        int endIndexBW = numOfVertices * 2;
-        Edge newEdge = new Edge();
-        newEdge.setVertexCount(numOfVertices);
-        newEdge.setAdjacencyMatrix(matrix1);
-        int t = 0;
-        for (int i = startIndexBW; i < endIndexBW; i++) {
-            for (int j = 0; j < numOfVertices; j++) {
-
-
-                if (matrix1[i][j] > 0) {
-
-                   // int BW = matrix1[i][j];
-                    int BW = 0;
-                    BW=500;
-                 //   alledges.add(newEdge.addEdge(t, i - 28, j, 1, matrix1[i][j]));
-                   alledges.add(newEdge.addEdge(t, i - 28, j, 1, BW));
-
-                    t++;
-                    matrix2[i][j] = BW;
-
-                }
-
-            }
-            ////System.out.println("");
+        int[][] matrixNew = rd.readfiles(filename);
+        int numOfVertices = matrixNew[0].length;
+        ArrayList<Vertex> allV = new ArrayList<Vertex>();
+        int id = 0;
+        int startIndexCPU = numOfVertices * 2;
+        int endIndexCPU = numOfVertices * 3;
+        for (int i = startIndexCPU; i < endIndexCPU; i++) {
+            Vertex newVer = new Vertex();
+            newVer.setNumber(id);
+            newVer.setOfferedCPU(matrixNew[i][id]);  
+            allV.add(newVer);
+            id++;
         }
 
-
-        int startIndexSpectrum = numOfVertices * 4;
-        int endIndexSpectrum = numOfVertices * 5;
-
-        t = 0;
-        for (int i = startIndexSpectrum; i < endIndexSpectrum; i++) {
-            for (int j = 0; j < numOfVertices; j++) {
-
-
-                if (matrix2[startIndexBW][j] > 0) {
-
-                    int startIndex = matrix1[i][j];
-//                    //System.out.println("Start Index of Edge "+t+": "+startIndex + " and BW: "+matrix2[startIndexBW][j]);
-                    alledges.get(t).setStartIndex(startIndex);
-                    alledges.get(t).setEndIndex(startIndex + matrix2[startIndexBW][j]);
-                    t++;
-                }
-
-            }
-            startIndexBW++;
-            ////System.out.println("");
-        }
-
-
-//      for (int i = 0; i < alledges.size(); i++) {
-//          
-//          
-//              System.out.println("Edge number: " + alledges.get(i).getId()+ ">>>> Source: " + alledges.get(i).getSource() +
-//                      " Destination: " + alledges.get(i).getDestination() + " BW: " + alledges.get(i).getBW() + " start Index: "+ alledges.get(i).getStartIndex() + " End index: " 
-//                      + alledges.get(i).getEndIndex());            
-//        }
-
-        writeInFile newAdj = new writeInFile();
-
-        //newAdj.writeFileEdge(matrix2);
-
-//        for (graphEdge edge :
-//                alledges) {
-//            //System.out.println("Edge " + edge.getId() + ":\t BW:" + edge.getBW() + ", Source-Destination: " + edge.getSource() + " - " + edge.getDestination() + ", Start-End Index: " + edge.getStartIndex() + " - " + edge.getEndIndex());
-//        }
-
-        return alledges;
+    	return allV;
     }
+    
+    
+
 
     
 }
